aa45588afcf4283b8eff555f6282046bb72d343a
Chulwoo Jung <chulwoo@bnl.gov>
2021-07-18 02:58:58 -0400

    Still gpart not working

--- Grid/qcd/hmc/GenericHMCrunner.h
+++ Grid/qcd/hmc/GenericHMCrunner.h
@@ -166,32 +166,32 @@ private:
     std::cout << GridLogMessage << "LaplacianRat " << std::endl;
     LaplacianRatParams gpar(1),mpar(1);
 #if 0
-//working? NO
+//jky
     gpar.offset = 0.;
-    gpar.a0[0] = 1;
-    gpar.a1[0] = 0.;
-    gpar.b0[0] = 1.-0.5*Kappa;
-    gpar.b1[0] = 0.5*Kappa;
-    gpar.b2=0.;
-    mpar.offset = 0.;
-    mpar.a0[0] = 1.-0.5*Kappa;
-    mpar.a1[0] = 0.5*Kappa;
-    mpar.b0[0] = 1.;
-    mpar.b1[0] = 0.;
-    mpar.b2=0.;
-#else
-//maybe working
-    gpar.offset = 0.;
-    mpar.a0[0] = 1.;
-    mpar.a1[0] = 0;
-    gpar.b0[0] = 1;
+    gpar.a0[0] = 1.;
+    gpar.a1[0] = -0.;
+    gpar.b0[0] = 1.;
     gpar.b1[0] = 0.;
     gpar.b2=0.;
     mpar.offset = 0.;
     mpar.a0[0] = 1.+0.5*Kappa;
     mpar.a1[0] = -0.5*Kappa;
-    mpar.b0[0] = 1.-0.5*Kappa;
-    mpar.b1[0] = 0.5*Kappa;
+    mpar.b0[0] = 1.;
+    mpar.b1[0] = 0.;
+    mpar.b2=0.;
+#else
+//NOT WORKING
+    gpar.offset = 0.;
+    gpar.a0[0] = 1.;
+    gpar.a1[0] = -0.;
+    gpar.b0[0] = 1;
+    gpar.b1[0] = +0;
+    gpar.b2=0.;
+    mpar.offset = 0.;
+    mpar.a0[0] = 2.;
+    mpar.a1[0] = -1.;
+    mpar.b0[0] = 1.;
+    mpar.b1[0] = 0.;
     mpar.b2=0.;
 #endif
     std::cout << GridLogMessage << "gpar a0= " << gpar.a0 <<std::endl;
