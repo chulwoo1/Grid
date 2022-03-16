5bde60a4fb98518f102b69ab02b6be44035618d5
Chulwoo Jung <chulwoo@bnl.gov>
2022-03-16 01:46:59 -0400

    Checking in before adding Hamiltonian outputs

--- Grid/qcd/hmc/GenericHMCrunner.h
+++ Grid/qcd/hmc/GenericHMCrunner.h
@@ -167,10 +167,12 @@ private:
     std::cout << GridLogMessage << "LaplacianRat " << std::endl;
 //#include "DBW2_3.h.inc"
 //#include "Wilson10_3.h.inc"
-#include "g_x3_1.h.inc"
+//#include "g_x3_1.h.inc"
 //#include "g_x3_3_sym.h.inc"
-//#include "Wilson64.h.inc"
+#include "g_rat_1.h.inc"
+//#include "Wilson64_2.h.inc"
 //#include "32I2.8Gev.h.inc"
+//#include "32I3.1Gev_2.h.inc"
     gpar.tolerance=Parameters.MD.RMHMCCGTol;
     mpar.tolerance=Parameters.MD.RMHMCCGTol;
     std::cout << GridLogMessage << "gpar a0= " << gpar.a0 <<std::endl;
--- HMC/Mobius2p1fEOFA_31Gev.cc
+++ HMC/Mobius2p1fEOFA_31Gev.cc
@@ -170,9 +170,8 @@ int main(int argc, char **argv) {
   
   //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
-  //  typedef GenericHMCRunner<LeapFrog> HMCWrapper; 
-//  typedef GenericHMCRunner<ImplicitMinimumNorm2> HMCWrapper; 
-  typedef GenericHMCRunner<MinimumNorm2> HMCWrapper; 
+  typedef GenericHMCRunner<ImplicitMinimumNorm2> HMCWrapper; 
+//  typedef GenericHMCRunner<ForceGradient> HMCWrapper; 
 
   HMCparameters HMCparams;
 #if 1
