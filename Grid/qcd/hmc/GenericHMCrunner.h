475f0468719c0a6e08da8b91c0b580097d87ee70
Chulwoo Jung <chulwoo@bnl.gov>
2023-03-03 22:46:39 +0000

    Adding HMCparameters.xml

--- Grid/qcd/hmc/GenericHMCrunner.h
+++ Grid/qcd/hmc/GenericHMCrunner.h
@@ -154,7 +154,7 @@ private:
     // Can move this outside?
     typedef IntegratorType<SmearingPolicy> TheIntegrator;
     // Metric
-#if 0
+#if 1
     std::cout << GridLogMessage << "Trivial metric" << std::endl;
     TrivialMetric<typename Implementation::Field> Mtr;
     TheIntegrator MDynamics(UGrid, Parameters.MD, TheAction, Smearing, Mtr);
