9a04193ea893b782ca2ee7a0e9389ac729e480d6
Chulwoo Jung <chulwoo@bnl.gov>
2025-01-24 01:47:28 +0000

    Checking in before pulling mixed5

--- HMC/Mobius2p1p1fEOFA_4Gev.cc
+++ HMC/Mobius2p1p1fEOFA_4Gev.cc
@@ -581,8 +581,8 @@ int main(int argc, char **argv) {
 //#include<g_x3_2_3.h.inc>
 //#include<g_x3_2_pol4.h.inc>
 //#include<g_poly.h.inc>     
-#include<poly_try_3.h.inc>
-//#include<poly_try_cheb_1.h.inc>
+//#include<poly_try_3.h.inc>
+#include<poly_try_cheb_1.h.inc>
 //    LaplacianRatParams gpar(2),mpar(2);
 
 #if 0
